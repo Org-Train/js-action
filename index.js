7b5781ac507338e40c159b074196e8c8d3a11c57
Ikram Touir <touir.ikram.1993@ieee.org>
2024-12-10 09:09:59 +0100

    fix

--- index.js
+++ index.js
@@ -1,30 +1,29 @@
-const core = require('@actions/core')
-const  github  = require('@actions/github')
+const core = require("@actions/core");
+const github = require("@actions/github");
 
-
+// most @actions toolkit packages have async methods
 async function run() {
   try {
-    const token = core.getIDToken('token')
-    const title = core.getIDToken('title')
-    const body = core.getIDToken('body')
-    const assignees = core.getIDToken('assignees')
-    const octokit = github.getOctokit(token)
-    const response = await octokit.rest.issues.create(
-     // {owner: github.context.repo.owner, repo:github.context.repo.repo}
-     {...github.context.repo,
+    const token = core.getInput("token");
+    const title = core.getInput("title");
+    const body = core.getInput("body");
+    const assignees = core.getInput("assignees");
+
+    const octokit = github.getOctokit(token);
+
+    const response = await octokit.rest.issues.create({
+      // owner: github.context.repo.owner,
+      // repo: github.context.repo.repo,
+      ...github.context.repo,
       title,
-       body,
-       assignees: assignees ? assignees.split("\n") : undefined,
-      }
+      body,
+      assignees: assignees ? assignees.split("\n") : undefined,
+    });
 
-    )
-    core.setOutput('issue', response.data)
-
+    core.setOutput("issue", response.data);
   } catch (error) {
     core.setFailed(error.message);
   }
 }
 
-module.exports = {
-  run
-}
+run();
